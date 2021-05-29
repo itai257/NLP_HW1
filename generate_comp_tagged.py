26c894a0d67bcc5a8a27c38cb101f0f7ae4583aa
imalka <itai257@gmail.com>
2021-05-30 01:21:21 +0300

    generate_comp_tagged

---
+++ generate_comp_tagged.py
@@ -0,0 +1,58 @@
+from memm_viterbi import memm_viterbi
+import pickle
+def generate_comp_tagged(weights_path, comp_path, generated_file_path, feature2id_class_path):
+
+    with open(feature2id_class_path, 'rb') as input:
+        feature2id = pickle.load(input)
+
+
+    all_tags = feature2id.feature_statistics.tags_count_dict.keys()
+
+    words_tags_to_generate = []
+    with open(comp_path) as f:
+        for i in range(10):
+            line = f.readline()
+            splited_words = line.replace('\n', ' ').split(' ')
+            del splited_words[-1]
+            infer_tags = memm_viterbi(all_tags, line, weights_path, feature2id)
+            words_tags_to_generate.append((splited_words, infer_tags))
+
+    sentences = []
+    for line in words_tags_to_generate:
+        sentence_parts = []
+        for i in range(len(line[0])):
+            word, tag = line[0][i], line[1][i]
+            word_tag = "_".join([word, tag])
+            sentence_parts.append(word_tag)
+        sentence = " ".join(sentence_parts)
+        sentences.append(sentence)
+
+    all_text = "\n".join(sentences)
+    with open(generated_file_path, "w+") as f:
+        f.write(all_text)
+
+
+    # validating generated file
+
+    #with open(generated_file_path) as f:
+    #    with open(comp_path) as f_comp:
+    #        for line, line_comp in zip(f, f_comp):
+    #            splited_words = line.replace('\n', ' ').split(' ')
+    #            del splited_words[-1]
+    #            comp_words = line_comp.split(' ')
+    #            for word_idx in range(len(splited_words)):
+    #                cur_word, cur_tag = splited_words[word_idx].split('_')
+    #                if cur_word != comp_words[word_idx]:
+    #                    print("files are different")
+    #                    print("generated: {}".format(line))
+    #                    print("real: {}".format(line_comp))
+    #                    exit(1)
+
+
+weights_path = 'trained_weights/trained_weights_data_train1.pkl'
+comp_path = "data/comp1.words"
+#comp_path = "/datashare/hw1/comp1.words"
+generated_file_path = "generated_comp/comp1.wtag"   # where to dump the tagged file
+feature2id_class_path = 'trained_weights/feature2id_train1.pkl'
+
+generate_comp_tagged(weights_path, comp_path, generated_file_path, feature2id_class_path)
